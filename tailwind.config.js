8b0525932c4ae57f7af6bf541cd2673ae82b9cab
Stefan Gajić <stefan.gajic@nenasal.com>
2022-05-11 18:53:27 +0200

    Setting up the work environment - setting up the maximum widths of the container component on all screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,45 @@ module.exports = {
     "./dist/*.html"
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px'
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      spacing: {
+        3.5: '14px'
+      }
+    },
   },
   plugins: [
-    require('tailwindcss-debug-screens')
+    require('tailwindcss-debug-screens'),
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '668px'
+          },
+          '@screen md': {
+            maxWidth: '796px'
+          },
+          '@screen lg': {
+            maxWidth: '1052px'
+          },
+          '@screen xl': {
+            maxWidth: '1198px'
+          },
+          '@screen 2xl': {
+            maxWidth: '1198px'
+          }
+        }
+      })
+    }
   ],
 }
